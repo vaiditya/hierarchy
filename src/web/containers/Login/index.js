34d625d44aa6bccd34d6ea6435d312f02fb49542
Vaiditya Chauhan <vaidityachauhan@gmail.com>
2019-10-24 13:08:11 +0530

    feat(Login):Login page setup

---
+++ src/web/containers/Login/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Login = () => <>Login Container</>;
+
+export default Login;
--- src/web/pages/index.js
+++ src/web/pages/index.js
@@ -2,3 +2,4 @@ export { default as HomePage } from "./HomePage";
 export { default as ManagerPage } from "./ManagerPage";
 export { default as LeadPage } from "./LeadPage";
 export { default as DeveloperPage } from "./DeveloperPage";
+export { default as LoginPage } from "./LoginPage";
